017e3edee9b2ed62258dc90798fb835442948ef7
Vivaan1643 <vivaan.k.urs@gmail.com>
2024-12-17 16:44:55 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+height=float(input("Enter your height in cms"))
+weight =float(input("Enter your weight in kgs"))
+BMI = weight/(height/100)**2
+
+if BMI <= 18.4:
+    print ("You are underweight")
+elif BMI>= 24.9:
+    print("You are Healthy")
+elif BMI>=29.9:
+    print("You are overweight")
+elif BMI>= 34.9:
+    print("You are obese")
+elif BMI>= 39.9:
+    print("You are severely obese")               
